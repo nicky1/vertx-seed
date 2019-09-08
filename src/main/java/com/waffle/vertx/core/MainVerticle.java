5431c123f1cb981aa52e95e473657d55961f4bbb
易晓双 <yixiaoshuang@yeah.net>
2019-09-08 23:18:17 +0800

    1.使用log4j2日志

--- src/main/java/com/waffle/vertx/core/MainVerticle.java
+++ src/main/java/com/waffle/vertx/core/MainVerticle.java
@@ -1,15 +1,35 @@
 package com.waffle.vertx.core;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author yixiaoshuang
  * @date 2019-09-06 00:12
  */
+@Slf4j
 public class MainVerticle extends AbstractVerticle {
 
     @Override
     public void start() throws Exception {
         vertx.deployVerticle(MyFirstVerticle.class.getName());
+    }
+
+    /**
+     * 异步版本:不要在start方法中,阻塞等待其它的Verticle创建完成
+     *
+     * @param startFuture Future
+     */
+    @Override
+    public void start(Future<Void> startFuture) throws Exception {
+        //部署其它verticle
+        vertx.deployVerticle("com.waffle.vertx.core.MyFirstVerticle", res -> {
+            if (res.succeeded()) {
+                System.out.println("success");
+            } else {
+                System.out.println("failed");
+            }
+        });
     }
 }
--- src/main/java/com/waffle/vertx/core/MyFirstVerticle.java
+++ src/main/java/com/waffle/vertx/core/MyFirstVerticle.java
@@ -1,18 +1,19 @@
 package com.waffle.vertx.core;
 
 import io.vertx.core.AbstractVerticle;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author yixiaoshuang
  * @date 2019-09-05 23:07
  */
+@Slf4j
 public class MyFirstVerticle extends AbstractVerticle {
 
     @Override
-    public void start() throws Exception {
-        vertx.createHttpServer().requestHandler(req ->{
-            req.response().putHeader("content-type","text/plain")
-                    .end("hello world222");
-        }).listen(8080);
+    public void start() {
+        vertx.createHttpServer().requestHandler(req ->
+                req.response().putHeader("content-type", "text/plain")
+                        .end("hello world222")).listen(8080);
     }
 }
